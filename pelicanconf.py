1cb74bf7bad39cd35c4f0b532b90ddddf9ae37cf
Bart Vanherck <vherckbart@gmail.com>
2015-07-02 19:50:53 +0200

    fixed the disqus comments

--- pelicanconf.py
+++ pelicanconf.py
@@ -5,14 +5,16 @@ from __future__ import unicode_literals
 AUTHOR = u'Bart Vanherck'
 SITENAME = u'Software And Testing'
 SITEURL = 'http://bartvanherck.github.io'
+FIXEDSITEURL = 'http://bartvanherck.github.io'
 #SITEURL = 'http://localhost:8000'
+#SITEURL = 
 
 PATH = 'content'
 
 TIMEZONE = 'Europe/Paris'
 
 DEFAULT_LANG = u'en'
-
+DISQUS_SITENAME = "swandtesting"
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
